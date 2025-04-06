ebb5addd9efe5e5d9b771559f1abc5df5c194824
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-04-06 14:26:17 +0200

    solve: title case and reverse    string problems

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,8 @@
-function titleCase() {}
+function titleCase(str) {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
 
 module.exports = titleCase;
