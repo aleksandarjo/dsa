375f75a04807de4d597d1792de12566f602a0c6d
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-04-23 13:38:24 +0200

    solve: count vowels & remove duplicates

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels(str) {
+  const arr = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    if (arr.includes(str[i])) {
+      result.push(str[i]);
+    }
+  }
+
+  return result.length;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+// function removeDuplicates(arr) {
+//   const uniqueArr = new Set(arr);
+//   return uniqueArr;
+// }
+
+function removeDuplicates(arr) {
+  const uniqueArr = [];
+  for (i = 0; i < arr.length; i++) {
+    if (uniqueArr.includes(arr[i])) continue;
+    uniqueArr.push(arr[i]);
+  }
+
+  return uniqueArr;
+}
 
 module.exports = removeDuplicates;
