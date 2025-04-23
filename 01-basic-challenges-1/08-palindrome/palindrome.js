0db8f26b311d66602371a8cc2c43a3b3c4fa43a7
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-04-23 10:58:01 +0200

    solve: palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,9 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversedStr = formattedStr.split("").reverse().join("");
+  return formattedStr === reversedStr;
+}
 
 module.exports = isPalindrome;
+
+// /[^a-z0-9]/g
