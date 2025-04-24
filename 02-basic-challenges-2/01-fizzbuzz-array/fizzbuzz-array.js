b2bca710cea3fc424352e9dd76232ca7f9dc7b6d
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-04-24 09:59:39 +0200

    solve: fizzbuzz & array intersection

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const result = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("fizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("fizz");
+    } else if (i % 5 === 0) {
+      result.push("buzz");
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
 
 module.exports = fizzBuzzArray;
