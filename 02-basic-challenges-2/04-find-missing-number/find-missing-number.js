0fa3a72e41c82bb368541fa571ab933d1877dda3
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-04-25 11:30:18 +0200

    solve: display likes & find missing number in array using gauss method

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (arr.length === 0) return 1;
+
+  const n = arr.length + 1;
+
+  const expectedSum = (n * (n + 1)) / 2;
+  const arrSum = arr.reduce((acc, i) => {
+    return acc + i;
+  }, 0);
+  return expectedSum - arrSum;
+}
 
 module.exports = findMissingNumber;
