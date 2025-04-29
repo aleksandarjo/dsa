f4d6622502aa7207e9825f9c7a06553450255514
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-04-29 10:16:47 +0200

    solve: are-all-chars-unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,12 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const unique = new Set([]);
+
+  for (let i = 0; i < str.length; i++) {
+    if (unique.has(str[i])) return false;
+    unique.add(str[i]);
+  }
+
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
