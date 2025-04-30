6ee7fb2cd40b4942200d60ca20b82e0fcef2afc2
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-04-30 10:31:22 +0200

    solve: dice-game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,32 @@
-function diceGameSimulation() {}
+const WINNING_SUMS = [7, 11];
+const LOSING_SUMS = [2, 3, 12];
+
+function diceGameSimulation(numSimulations) {
+  const rolls = [];
+
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+    const sum = dice1 + dice2;
+    let result;
+
+    if (WINNING_SUMS.includes(sum)) {
+      result = "win";
+    } else if (LOSING_SUMS.includes(sum)) {
+      result = "lose";
+    } else {
+      result = "roll again";
+    }
+
+    rolls.push({
+      dice1,
+      dice2,
+      sum,
+      result,
+    });
+  }
+
+  return rolls;
+}
 
 module.exports = diceGameSimulation;
