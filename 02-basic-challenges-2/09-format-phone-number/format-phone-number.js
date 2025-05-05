1a5c645baacf5c644bc5d1117c8efec394e58647
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-05-05 10:26:23 +0200

    solve: format-phone-number & validate-email

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,8 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  const formatted = arr.join("");
+  return `(${formatted.slice(0, 3)}) ${formatted.slice(3, 6)}-${formatted.slice(
+    -4
+  )}`;
+}
 
 module.exports = formatPhoneNumber;
