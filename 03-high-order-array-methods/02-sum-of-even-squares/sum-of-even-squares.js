9f7c425fc83667293e084cbad0a457ba987a1e54
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-05-06 10:32:06 +0200

    solve: sum-of-even-squares & calculate-total-sales

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,10 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+  const result = arr
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * num)
+    .reduce((acc, num) => acc + num, 0);
+
+  return result;
+}
 
 module.exports = sumOfEvenSquares;
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,10 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, tax) {
+  const total = products
+    .map((item) => item.price * item.quantity)
+    .reduce((acc, item) => acc + item, 0);
+
+  const totalTax = total * (1 + tax / 100);
+  return totalTax;
+}
 
 module.exports = calculateTotalSalesWithTax;
