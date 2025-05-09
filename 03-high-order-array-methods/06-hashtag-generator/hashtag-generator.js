4c433ccdb9b7632373215528f7282d2fcd454b06
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-05-09 13:09:21 +0200

    solve: hashtag-generator

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,13 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.length === 0 || str.length >= 140) return false;
+
+  const hashtag = str
+    .trim()
+    .split(/\s+/)
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join("");
+
+  return `#${hashtag}`;
+}
 
 module.exports = generateHashtag;
