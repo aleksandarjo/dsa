631d37518752647c746bc445b45f7ce61ab9206f
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-05-13 10:56:54 +0200

    solve: analyze-car-milage

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,30 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((acc, car) => acc + car.mileage, 0);
+
+  const highestMileageCar = cars.reduce((acc, car) => {
+    if (acc.mileage > car.mileage) {
+      return acc;
+    } else {
+      return car;
+    }
+  }, cars[0]);
+
+  const lowestMileageCar = cars.reduce((acc, car) => {
+    if (acc.mileage < car.mileage) {
+      return acc;
+    } else {
+      return car;
+    }
+  }, cars[0]);
+
+  const averageMileage = totalMileage / cars.length;
+
+  return {
+    averageMileage,
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
