10c5b05a4766851943016ea90093853a66b7f3f7
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-05-14 09:51:31 +0200

    solve: password-validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,18 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const isLengthValid = password.length >= 8;
+
+  const hasUpperCase = password
+    .split("")
+    .some((char) => char === char.toUpperCase());
+
+  const hasLowerCase = password
+    .split("")
+    .some((char) => char === char.toLowerCase());
+
+  const hasDigit = password.split("").some((char) => char == parseInt(char));
+
+  if (isLengthValid && hasUpperCase && hasLowerCase && hasDigit) return true;
+  return false;
+}
 
 module.exports = validatePassword;
