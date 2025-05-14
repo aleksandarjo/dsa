2dab5c894c30d0e6e2f6ec37144a3ad2b8c457ee
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-05-14 09:58:54 +0200

    solve?: password-validator

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -3,13 +3,15 @@ function validatePassword(password) {
 
   const hasUpperCase = password
     .split("")
-    .some((char) => char === char.toUpperCase());
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
 
   const hasLowerCase = password
     .split("")
-    .some((char) => char === char.toLowerCase());
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
 
-  const hasDigit = password.split("").some((char) => char == parseInt(char));
+  const hasDigit = password
+    .split("")
+    .some((char) => !isNaN(parseInt(char, 10)));
 
   if (isLengthValid && hasUpperCase && hasLowerCase && hasDigit) return true;
   return false;
