bf6fe84c2b865273d44764c827cc7e1ba16298e9
Aleksandar <aleksandar.joovanoovic@gmail.com>
2025-05-19 10:44:56 +0200

    solve: reverse-string-recursion

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,7 @@
-function sumUpTo() {}
+function sumUpTo(num) {
+  if (num === 1) return 1;
+
+  return num + sumUpTo(num - 1);
+}
 
 module.exports = sumUpTo;
--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,8 @@
-function reverseString() {}
+function reverseString(string) {
+  if (string === "") return "";
+
+  console.log(string.substring(1));
+  return reverseString(string.substring(1)) + string[0];
+}
 
 module.exports = reverseString;
