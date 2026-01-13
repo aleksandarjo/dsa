f71d04960f7bbe605d8b8401ab5852f1691a9b6d
Aleksandar <aleksandar.joovanoovic@gmail.com>
2026-01-13 11:49:59 +0100

    solve: fibonacci

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,7 +1,9 @@
-function fibonacci(num) {
-  if (num < 2) return num;
+function fibonacci(n) {
+  if (n === 0) return 0;
 
-  return fibonacci(num - 1) + fibonacci(num - 2);
+  if (n === 1) return 1;
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci;
