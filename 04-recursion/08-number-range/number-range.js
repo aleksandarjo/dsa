5d122674c8c190fc06381ff823ec1d3ff8c15439
Aleksandar <aleksandar.joovanoovic@gmail.com>
2026-01-30 12:06:31 +0100

    failed: number range recursive

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,7 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (startNum === endNum) return [endNum];
+
+  return [startNum, ...numberRange(startNum + 1, endNum)];
+}
 
 module.exports = numberRange;
