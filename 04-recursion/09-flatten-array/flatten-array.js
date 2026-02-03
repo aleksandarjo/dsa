6bc52e92ba8f275d02e408afc3508987baa9ff08
Aleksandar <aleksandar.joovanoovic@gmail.com>
2026-02-03 11:37:53 +0100

    failed: array flatten recursive

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,17 @@
-function flattenArray() {}
+function flattenArray(arr) {
+  const result = [];
+
+  for (let i of arr) {
+    if (Array.isArray(i)) {
+      result.push(...flattenArray(i));
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
 
 module.exports = flattenArray;
+
+flattenArray([1, 2, 3, [4, 5, [6, 7, 8], 9], 10]);
