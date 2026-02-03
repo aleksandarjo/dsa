a09552984078cd24d123ef1a055f3c1b4a0cf133
Aleksandar <aleksandar.joovanoovic@gmail.com>
2026-02-03 13:30:15 +0100

    failed: permutations recursive

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,11 @@
-function permutations() {}
+function permutations(str) {
+  const arr = str.split("");
+
+  for (let i = 0; i < arr.length; i++) {
+    const result = arr[i] + permutations(...);
+  }
+}
 
 module.exports = permutations;
+
+permutations("abc");
